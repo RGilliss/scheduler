77754c230bd91144a09213c6a59585ca7a283835
Rye Gilliss <gilliss.rg@gmail.com>
2021-03-16 09:47:55 -0600

    day 1 components - button, DayList, DayListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function Button(props) {
+  let name;
+ 
+  const interviewerClass = classNames("interviewers__item", {
+    //  "-image": props.avatar,
+    "interviewers__item--selected": props.selected
+  });
+  if (props.selected) {
+    name = props.name
+  }
+
+  return (
+    <li className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      {name}
+    </li>
+  
+  );
+};
