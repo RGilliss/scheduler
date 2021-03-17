97ef5cb64b990b128e466ff44b3c3d9fedce8b23
Rye Gilliss <gilliss.rg@gmail.com>
2021-03-17 17:25:20 -0600

    useState refactor

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,16 +52,22 @@ const appointments = [
     }
   }
 ];
-
 export default function Application() {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {}
+  }); 
+
   useEffect(() => {
     const daysURL = `/api/days`;
     axios.get(daysURL).then((response) => {
      setDays(response.data)
     });
   }, [])
+
   const schedule = appointments.map(appointment => {
     return (
       <Appointment 
@@ -79,8 +85,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
