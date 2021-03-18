da612d56525a585f516fc60b3143b5cf692490a3
Rye Gilliss <gilliss.rg@gmail.com>
2021-03-18 09:53:37 -0600

    dry up code

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,13 +10,11 @@ export default function Application() {
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState(prev => ({ ...prev, days}));
 
-  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   }); 
-  
   
   useEffect(() => {
     Promise.all([
@@ -25,48 +23,43 @@ export default function Application() {
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
     })
-  }, [])
+  }, []);
   
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  console.log("dailyAppointments", dailyAppointments);
-  // const schedule = dailyAppointments.map(appointment => {
-  //   console.log("Appointment",appointment)
-  //   return (
-  //     <Appointment 
-  //     key={appointment.id} time={appointment.time}
-  //     />
-  //     )})
-      return (
-        <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            day={state.day}
-            setDay={setDay}
-          />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {dailyAppointments.map(appointment => {
-    console.log("Appointment",appointment)
+
+  const schedule = dailyAppointments.map(appointment => { 
     return (
       <Appointment 
       key={appointment.id} time={appointment.time} interview={appointment.interview}
       />
-      )})}
-      </section>
-    </main>
+    )}
+  );
+
+    return (
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
+          />
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList
+              days={state.days}
+              day={state.day}
+              setDay={setDay}
+            />
+          </nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />
+        </section>
+        <section className="schedule">
+          {schedule}
+        </section>
+      </main>
   );
 }
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,5 @@
 export default function getAppointmentsForDay(state, day) {
   let apps = []
-  console.log("State.days:", state.days);
   for (const eachDay of state.days) {
     if (eachDay.name === day) {
       const appointments = eachDay.appointments;
@@ -11,16 +10,5 @@ export default function getAppointmentsForDay(state, day) {
   }
   return apps;
 }
-// export function getAppointmentsForDay(state, day) {
-//   let result = []
-//   const appDay = state.days.filter(eachDay => eachDay.name === day);
-//   const apps = appDay[0].appointments;
-//   for (let id of apps) {
-//     if (id == state.appointments[id].id) {
-//       result.push(state.appointments[id]);
-//     }
-//   };
-//   console.log("result", result)
-//   return result;
-// }
+
 
